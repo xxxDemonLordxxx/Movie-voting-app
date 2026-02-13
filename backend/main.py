0ff755c7bbacfaac6029677e78a0db7b91da7d09
xxxDemonLordxxx <kondanna128@gmail.com>
2026-02-13 16:10:43 +0200

    allow all

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://observational.website", "https://www.observational.website"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
